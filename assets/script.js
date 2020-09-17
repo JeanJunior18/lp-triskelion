19fd742bda700192221c94090052881a3746c508
Jean Junior <jclgjunior@gmail.com>
2020-09-17 14:45:25 -0300

    Whatsapp button and AOS effects

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Burguer NavBar
+function myFunction() {
+    var x = document.getElementById("sections");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
